0267d9e0ec055424989ad0c9efe7c629f6fefd81
zj <zdoristup@gmail.com>
2026-09-13 12:24:31 +0800

    XX

--- src/device.c
+++ src/device.c
@@ -3,6 +3,11 @@
 
 void show_device_status(void)
 {
-	printf("Device Monitor v1.0 Start\n");
+	
+	printf("Device Monitor v1.1 Start\n");
 }
 
+void show_device_ip(const char *ip)
+{
+	printf("IP:%s\n",ip);
+}
--- src/main.c
+++ src/main.c
@@ -5,5 +5,6 @@ int main()
 {
     printf("Device Monitor Start\n");
     show_device_status();
+	show_device_ip("192.168.1.100");
     return 0;
 }
